c977a0924a6f7e71360040cf37e60ce38e182e72
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-07 20:58:27 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from 'components';
+
+const HomePage = lazy(() => import('../../pages/Home/Home'));
+const RegisterPage = lazy(() => import('../../pages/Register/Register'));
+const LoginPage = lazy(() => import('../../pages/Login/Login'));
+const ContactsPage = lazy(() => import('../../pages/Contacts/Contacts'));
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="*" element={<>NotFound </>} />
+      </Route>
+    </Routes>
+  );
+}
