4367fe768102048d5a88af84ff00f2b632620b0f
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-08 23:09:52 +0300

    Update Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,10 @@
+import { RegisterForm } from 'components';
+
 export default function Register() {
-  return <>Register Page</>;
+  return (
+    <div>
+      <h2>Registration</h2>
+      <RegisterForm />
+    </div>
+  );
 }
