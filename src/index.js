b8934bbab2df82feb8a4e72a7ca96c17dd7efefb
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-09 14:01:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/peronal-phonebook">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/peronal-phonebook">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
