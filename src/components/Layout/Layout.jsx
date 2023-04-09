43b300153001bf24697cf0d572392ed245616846
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-09 21:58:25 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
-import { Header } from 'components';
-import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from 'components';
 
 export function Layout() {
   return (
