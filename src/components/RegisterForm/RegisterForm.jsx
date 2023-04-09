75fac5cb916cc71b227b2de344580818a5e5a0d6
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-09 21:58:16 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import {
   Formik,
   Form,
   Field,
   // ErrorMessage
 } from 'formik';
-import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 
 export function RegisterForm() {
@@ -25,19 +25,15 @@ export function RegisterForm() {
       >
         <Form>
           <label htmlFor="name">User name:</label>
-          <Field type="text" name="name" placeholder="Enter user name"></Field>
+          <Field type="text" name="name" placeholder="Enter user name" />
           {/* <ErrorMessage name="name" component="span"></ErrorMessage> */}
 
           <label htmlFor="email">Email:</label>
-          <Field type="email" name="email" placeholder="Enter email"></Field>
+          <Field type="email" name="email" placeholder="Enter email" />
           {/* <ErrorMessage name="email" component="span"></ErrorMessage> */}
 
-          <label htmlFor="password">Password: </label>
-          <Field
-            type="password"
-            name="password"
-            placeholder="Enter password"
-          ></Field>
+          <label htmlFor="password">Password:</label>
+          <Field type="password" name="password" placeholder="Enter password" />
           {/* <ErrorMessage name="password" component="span"></ErrorMessage> */}
 
           <button type="submit">Register</button>
