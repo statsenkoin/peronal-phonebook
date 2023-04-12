57c386e0490b5e1d26f5f76272d74b8c2834a395
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-12 22:35:57 +0300

    Create Home.styled.jsx

---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const HomePage = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const HomeContent = styled.div`
+  font-size: 32px;
+  font-weight: 500;
+  color: '#010101';
+`;
