00d79e6dc0c0fe216d70cd15a4f202ca6d4c7816
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-12 21:37:47 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import { AppNav, AuthNav, UserMenu } from 'components';
 import { useAuth } from 'hooks';
+import { HeaderBar, NavBar } from './Header.styled';
 
 export function Header() {
   const { isLoggedIn } = useAuth();
   return (
-    <div>
-      <AppNav />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </div>
+    <HeaderBar>
+      <NavBar>
+        <AppNav />
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </NavBar>
+    </HeaderBar>
   );
 }
