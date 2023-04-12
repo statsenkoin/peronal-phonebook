72cb3797c404044723e87d98e5d0fe2ee6575f20
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-12 22:36:03 +0300

    Update UserMenu.styled.jsx

--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -4,25 +4,31 @@ export const Nav = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  align-items: center;
+  align-items: baseline;
 `;
 
 export const UserName = styled.p`
-  margin: 5px 10px;
+  margin: 0 10px;
   font-size: 16px;
   text-decoration: none;
-  text-transform: uppercase;
   font-weight: 700;
   color: #fff;
 `;
 
 export const LogoutBtn = styled.button`
   padding: 5px 10px;
-  font-size: 12px;
-  text-transform: uppercase;
+  font-size: 14px;
   font-weight: 700;
   color: #fff;
   background-color: #42a5f5;
-  border: 1px solid #1769aa;
+  border-width: 0;
   border-radius: 4px;
+
+  transition: 300ms;
+  &:hover,
+  &:focus {
+    color: #ff9800;
+    background-color: #4dabf5;
+    cursor: pointer;
+  }
 `;
