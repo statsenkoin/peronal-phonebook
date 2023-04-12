9ff8cc07f9eff3663c314f2229bfc4fc9092e001
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-12 21:54:21 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,39 @@
 import { AppNav, AuthNav, UserMenu } from 'components';
 import { useAuth } from 'hooks';
 
+import { AppBar, Container, Grid } from '@mui/material';
+
 export function Header() {
   const { isLoggedIn } = useAuth();
   return (
-    <div>
-      <AppNav />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </div>
+    <AppBar position="static">
+      <Container maxWidth="sm">
+        <Grid
+          container
+          minHeight={96}
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid
+            display="flex"
+            // justifyContent="flex-start"
+            alignItems="center"
+            gap={2}
+          >
+            <AppNav />
+          </Grid>
+          <Grid
+            display="flex"
+            // justifyContent="flex-end"
+            alignItems="center"
+            gap={2}
+          >
+            {isLoggedIn ? <UserMenu /> : <AuthNav />}
+          </Grid>
+        </Grid>
+      </Container>
+    </AppBar>
   );
 }
